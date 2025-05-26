44c3cfa075bd56d1b6c60c6cb842e699afc49bd2
Rococcoon <spacedangles@gmail.com>
2025-05-25 18:24:14 -0700

    task: remove tailwind

--- minify-tailwindcss.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Compile and minify your CSS for production
-./tailwindcss -i assets/css/main.css -o static/css/main.css --minify
--- watch-tailwindcss.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start a watcher
-./tailwindcss -i assets/css/main.css -o static/css/main.css --watch
